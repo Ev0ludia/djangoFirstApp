ebf86c02878526188fd8d69c78da31b24ee1246a
Ev0ludia <aurelien@evoludia.com>
2022-06-22 10:57:53 +0200

    Ajout de vues et de models, découverte des migrations et du shell

---
+++ myFirstHello/listings/views.py
@@ -0,0 +1,36 @@
+from typing import List
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from listings.models import Band
+from listings.models import Listing
+
+def hello(request):
+    bands = Band.objects.all()
+    return HttpResponse(f"""
+        <h1>Hello Django !</h1>
+        <p>Liste des groupes<p>
+        <ul>
+            <li>{bands[0].name}</li>
+            <li>{bands[1].name}</li>
+            <li>{bands[2].name}</li>
+        </ul>    
+        
+    """)
+
+def about(request):
+    return HttpResponse('About-us')    
+    
+def listings(request):
+    listings = Listing.objects.all()
+    return HttpResponse(f"""
+        <h1>Listings</h1>
+        <p>Listing</p>
+        <ul>
+            <li>{listings[0].title}</li>
+            <li>{listings[1].title}</li>
+        </ul>
+    """)  
+
+def contact(request):
+    return HttpResponse('Contact us')    
